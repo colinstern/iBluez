9ba5f932c8a9e90c886e9c31ae7ba58bfa638842
cost <cstern19@gmail.com>
2016-08-03 15:25:21 -0400

    Now writes to file on internal storage.

--- app/src/main/java/sensors/ibluez/ConnectedThread.java
+++ app/src/main/java/sensors/ibluez/ConnectedThread.java
@@ -1,8 +1,14 @@
 package sensors.ibluez;
 
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,6 +21,8 @@ class ConnectedThread extends Thread {
     private final InputStream mmInStream;
     private final OutputStream mmOutStream;
     private final Handler mHandler;
+    FileOutputStream outputStream;
+    Context context;
 
     public ConnectedThread(BluetoothSocket socket, Handler handler) {
         mHandler = handler;
@@ -36,25 +44,41 @@ class ConnectedThread extends Thread {
     }
 
     public void run() {
+        sendMessageToMainActivity("Running ConnectedThread");
         byte[] buffer = new byte[1024];  // buffer store for the stream
         int bytes; // bytes returned from read()
+        File storageDir = getStorageDir();
+        String filename = "data_1";
+        File file = new File(storageDir, filename);
+        sendMessageToMainActivity("Created new file");
 
-        sendMessageToMainActivity("Running ConnectedThread");
-        // Keep listening to the InputStream until an exception occurs
-        while (true) {
+        try {
             try {
-                // Read from the InputStream
-                sendMessageToMainActivity("Preparing to read data");
-                bytes = mmInStream.read(buffer);
-                sendMessageToMainActivity("Data read");
-                // Send the obtained bytes to the UI activity
-                mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer)
-                        .sendToTarget();
-                sendMessageToMainActivity("Read 1024 bytes");
-            } catch (IOException e) {
-                sendMessageToMainActivity("Connection terminated: " + e.toString());
-                break;
+                outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+
             }
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    sendMessageToMainActivity("Preparing to read data");
+                    bytes = mmInStream.read(buffer);
+                    sendMessageToMainActivity("Data read");
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                    outputStream.write(buffer);
+                    sendMessageToMainActivity("Read 1024 bytes");
+                } catch (IOException e) {
+                    sendMessageToMainActivity("Connection terminated: " + e.toString());
+                    break;
+                }
+            }
+            outputStream.close();
+        } catch (Exception e) {
+            sendMessageToMainActivity("IO error!" + e.toString());
         }
     }
 
@@ -81,5 +105,34 @@ class ConnectedThread extends Thread {
         // Send the obtained bytes to the UI activity
         mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer)
                 .sendToTarget();
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getStorageDir() {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), "iShadow Received Data");
+        if (!file.mkdirs()) {
+            sendMessageToMainActivity("Directory not created");
+        }
+        return file;
     }
 }
